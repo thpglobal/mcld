3a9ddd939073241f9e04e5a951aec6d9d819d204
John Coonrod <john.coonrod@thp.org>
2021-12-22 13:19:01 -0500

    remove extra footers

--- footer.php
+++ footer.php
@@ -1,30 +1,13 @@
 <?php
 /**
  * The template for displaying the footer
- *
+ * Now that blocks can go into widgets, no reason to have multiple footer areas
  */
 ?>
 </div> <!-- close the page container -->
 
-<div class="actionbar">
-	<?php dynamic_sidebar( 'call_to_action' ); ?>
-</div>
-<footer class="site-footer">
-<div class="wp-block-columns">
-  <div class="wp-block-column footer1">
-	<?php dynamic_sidebar( 'footer_area_one' ); ?>
-  </div>
-  <div class="wp-block-column footer2">
-	<?php dynamic_sidebar( 'footer_area_two' ); ?>
-  </div>
-  <div class="wp-block-column footer3">
-	<?php dynamic_sidebar( 'footer_area_three' ); ?>
-  </div>
-</div>
-	<p>&nbsp;</p>
-
-	<?php dynamic_sidebar( 'footer_area_four' ); ?>
-
+<footer>
+	<?php dynamic_sidebar( 'footer_area' ); ?>
 </footer>
 <?php wp_footer(); ?>
 </body>
