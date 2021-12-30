b5eceaf6b9bed32d86806e4a9e87ed0cd90bd84f
John Coonrod <john.coonrod@thp.org>
2021-12-30 13:39:56 -0500

    fix up look

--- single.php
+++ single.php
@@ -9,37 +9,29 @@ get_header();
 
 <main id="main" class="site-main" role="main">
 
-	<?php while ( have_posts() ) : the_post(); ?>
+<?php while ( have_posts() ) : the_post(); ?>
 
-<article id="post-<?php the_ID(); ?>" class="entry">
-	<header class="entry-header">
-		<h1><?php the_title(); ?></h1>
-		<h4><?php the_date(); ?></h4>
-		
-	</header>
+	<article id="post-<?php the_ID(); ?>" class="entry">
+		<header class="entry-header">
+			<h1><?php the_title(); ?></h1>
+			<h4><?php the_date(); ?></h4>
+		</header>
 
-	<div class="entry-content wp-block-columns">
-	  <div class="wp-block-column" style="flex-basis:70%">
-		  <?php if ( has_post_thumbnail() ) { 
-			$thpurl=get_the_post_thumbnail_url();
-			echo("<img src='$thpurl' width=100% height=auto>");
-		} 
-		  the_content(); ?>
+		<div class="entry-content wp-block-columns">
+			<div class="wp-block-column" style="flex-basis:70%;">
+				<?php if ( has_post_thumbnail() ) { 
+						$thpurl=get_the_post_thumbnail_url();
+						echo("<img src='$thpurl' width=100% height=auto>");
+					} 
+			  	the_content(); ?>
+			</div>
+		<div class="wp-block-column" style="flex-basis:30%;">
+			<?php dynamic_sidebar( 'sidebar-1' );?>
 		</div>
-	  <div class="wp-block-column" style="flex-basis:30%">
-		  <?php dynamic_sidebar( 'sidebar-1' );?>
 	</div>
-</article>
-<?php
-	// If comments are open or we have at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) :
-		comments_template();
-	endif;
-
-	endwhile;
-	?>
+	</article>
+	<?php	endwhile; ?>
 
 </main>
-
-<?php
+<?php 
 get_footer();
