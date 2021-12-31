a41a09170f8fc5db7d26f17a7fc8fe89fcac84e0
John Coonrod <john.coonrod@thp.org>
2021-12-31 09:01:02 -0500

    fix translate code

--- footer.php
+++ footer.php
@@ -9,5 +9,7 @@
 	<?php dynamic_sidebar( 'footer_area' ); ?>
 </footer>
 <?php wp_footer(); ?>
+<script>
+function googleTranslateElementInit() { new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');}</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 </body>
 </html>
