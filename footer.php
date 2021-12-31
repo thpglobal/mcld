46431425543ae2a846ab93d9c71db7cc1a8a8801
John Coonrod <john.coonrod@thp.org>
2021-12-31 16:46:50 -0500

    fix img tag in css

--- footer.php
+++ footer.php
@@ -10,6 +10,7 @@
 </footer>
 <?php wp_footer(); ?>
 <script>
-function googleTranslateElementInit() { new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');}</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+function googleTranslateElementInit() { new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');}</script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 </body>
 </html>
